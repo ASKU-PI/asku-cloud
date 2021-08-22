e370ed6f5ef977f79076e64b9b48cb05ca92372b
Szymon Świerk <swierk.szymon99@gmail.com>
2021-08-22 23:09:29 +0200

    refactor

--- gateway/src/main/java/pl/asku/gateway/config/SwaggerConfig.java
+++ gateway/src/main/java/pl/asku/gateway/config/SwaggerConfig.java
@@ -17,17 +17,17 @@ public class SwaggerConfig implements SwaggerResourcesProvider {
     @Override
     public List<SwaggerResource> get() {
         List<SwaggerResource> resources = new ArrayList<>();
-        resources.add(swaggerResource("asku-auth-service", "/auth/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("asku-account-service", "/account/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("asku-magazine-service", "/magazine/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("asku-auth-service", "/auth/v2/api-docs"));
+        resources.add(swaggerResource("asku-account-service", "/account/v2/api-docs"));
+        resources.add(swaggerResource("asku-magazine-service", "/magazine/v2/api-docs"));
         return resources;
     }
 
-    private SwaggerResource swaggerResource(String name, String location, String version) {
+    private SwaggerResource swaggerResource(String name, String location) {
         SwaggerResource swaggerResource = new SwaggerResource();
         swaggerResource.setName(name);
         swaggerResource.setLocation(location);
-        swaggerResource.setSwaggerVersion(version);
+        swaggerResource.setSwaggerVersion("2.0");
         return swaggerResource;
     }
 
